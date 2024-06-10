77467fbc316772cf5417449ddb4d8e83fe4f88b2
debugdawn <90399309+debugdawn@users.noreply.github.com>
2024-06-10 08:46:31 +0200

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,40 @@
+<?php
+$firstname = filter_input(INPUT_POST, 'firstname');
+$lastname = filter_input(INPUT_POST, 'lastname');
+$email = filter_input(INPUT_POST, 'email');
+$password = filter_input(INPUT_POST, 'password');
+if (!empty($email)){
+	if (!empty(password)){
+	$host = "localhost";
+	$dbusername = "root";
+	$dbpassword = "";
+	$dbname = "glazedskincare";
+
+	//Create connection
+ $conn = new mysqli ($host,$dbusername,$dbpassword, $dbname);
+
+ if (mysqli_connect_error()){
+ 	die('Connect Error ('. mysql_connect_errno().')'
+ 		.mysqli_connect_error());
+ }
+ else{
+ 	$sql = "INSERT INTO account (email,password) values ('$email','$password') " ;
+ 	if ($conn->query($sql)){
+ 		echo "New record is inserted successfully"
+ 	}
+ 	else{
+ 		echo "Error: ".$sql."<br>".$conn->error;
+ 	}
+ 	$conn->close();
+ }
+	}
+	else{
+	echo "Password should not be empty";
+	die();
+	}
+}
+else{
+	echo "Email should not be empty";
+	die();
+	}
+?>
